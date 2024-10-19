bcad16094a464ea53b1a0e5fc03bdc9a31fd62f0
shubham kumari <kumarishubham273@gmail.com>
2024-10-19 20:20:11 +0530

    cv

--- src/Components/Cv.jsx
+++ src/Components/Cv.jsx
@@ -8,7 +8,7 @@ import github from '../assets/images/Github.svg';
 import linkedin from '../assets/images/Vector.svg';
 import line2 from '../assets/images/Line 1.svg';
 
-const PDF_FILE_URL = 'https://drive.google.com/file/d/15J0q9MeDwCmXaRj8-bVMxiGEUVOCAyVV/view?usp=drive_link';
+const PDF_FILE_URL = 'https://drive.google.com/file/d/11Pbm4EXM265gnijcdwun8uuFaxbpPxBa/view?usp=drive_link';
 
 const Cv = () => {
     const { ref: experienceRef, inView: experienceInView } = useInView({ threshold: 0.1, });
