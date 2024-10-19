53a020a181338785e7271c030995ea668cf0307f
shubham kumari <kumarishubham273@gmail.com>
2024-10-19 20:43:28 +0530

    projects

--- src/Components/Projects2.jsx
+++ src/Components/Projects2.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import car from "../assets/images/car.png";
 import skillzyy from "../assets/images/skillzyy (1).png";
-import vb from "../assets/images/VB_ITR.png";
+import PROJECTMANAGEMENT from "../assets/images/PROJECT-MANAGEMENT.png";
+import REACTQUIZ from "../assets/images/REACT-QUIZ.png";
+
 
 const image = [
   {
@@ -13,14 +15,18 @@ const image = [
     link: "https://www.skillzyy.in/",
   },
   {
-    img: vb,
-    link: "https://www.itrchandigarh.org/",
+    img: PROJECTMANAGEMENT,
+    link: "https://project-management-app-five-psi.vercel.app/",
+  },
+  {
+    img: REACTQUIZ,
+    link: "https://quiz-drab-alpha.vercel.app/",
   },
 ];
 
 const Projects2 = () => {
   return (
-    <div className="font-mon font-medium sm:text-base text-sm text-900 lg:px-24 px-5 sm:py-16 py-8 flex flex-col sm:gap-16 gap-8 items-center justify-center">
+    <div className="font-mon font-medium sm:text-base text-sm text-900 lg:px-24 px-5 sm:py-16 py-8 flex flex-col sm:gap-16 gap-8 items-center justify-center border-b border-99 h-screen">
       <div className="flex py-6 justify-center items-center self-stretch font-dm text-primary sm:text-8xl text-5xl text-center rounded-full bg-900">
         Frontend Projects
       </div>
@@ -35,7 +41,7 @@ const Projects2 = () => {
                 <img
                   src={e.img}
                   alt="Project"
-                  className="rounded-lg w-full h-auto"
+                  className="rounded-lg w-72 h-72"
                 />
                 <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 flex items-center justify-center rounded-lg transition-opacity duration-300">
                   <a
